f04f92356f0849e7d58ee0f8950ff5fd7d28acee
JonnyNineToes <bustedglass@gmail.com>
2015-02-14 16:20:01 -0600

    just some cleaning up

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
+
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title></title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title></title>
+		
 		<!--<link rel="stylesheet" href="css/diagnostic.css">-->
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/global.css">
@@ -12,28 +15,33 @@
 		<link rel="stylesheet" href="css/nav.css">
 		<link rel="stylesheet" href="css/main.css">
 		<link rel="stylesheet" href="css/footer.css">
+		
 		<style type="text/css">
 			
 
-			@media only screen and (min-width:581px) {
+			@media only screen and ( min-width: 581px ) {
 				
 			}
 
-			@media only screen and (min-width:891px) {
+			@media only screen and ( min-width: 891px ) {
 				
 			}
 			
-			@media only screen and (min-width:1121px) {
+			@media only screen and ( min-width: 1121px ) {
 				
 			}
 		</style>
 	</head>
+
 	<body>
 		<header>
 			<h1>
-				<a href=""><img src="img/" alt=""></a>
+				<a href="">
+					<img src="img/" alt="">
+				</a>
 			</h1>
 		</header>
+		
 		<nav>
 			<ul>
 				<li id="">
@@ -41,16 +49,21 @@
 				</li>
 			</ul>
 		</nav>
+		
 		<main>
 			<section>
 				<header>
 					<h2></h2>
 				</header>
+				
+				<p></p>
 			</section>
 		</main>	
+		
 		<footer>
 			<p>&copy; <?php echo date('Y'); ?> All Rights Reserved</p>
 		</footer>
+		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="/js/jquery-2.1.1.min.js"><\/script>')</script>
 		<!--<script src="js/mobilize.js"></script>-->
